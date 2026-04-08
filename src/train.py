2cd28811a064c3b745d5b7b0060dc8310105784d
Oleh Shuptar <01193038@pw.edu.pl>
2026-04-08 13:09:37 +0200

    feat: added criteria for model evaluation on test dataset

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch
 
-def train_model(model : nn.Module, train_loader, val_loader, criterion, optimizer, device, epochs):
+def train_model(model : nn.Module, train_loader, test_loader, criterion, optimizer, device, epochs):
     model.to(device)
     for epoch in range(epochs):
         model.train()
@@ -17,7 +17,12 @@ def train_model(model : nn.Module, train_loader, val_loader, criterion, optimize
             optimizer.step()
             total_loss += loss.item()
         
+        mae = evaluate_model(model, test_loader, mae_criterion, device)
+        rmse = evaluate_model(model, test_loader, rmse_criterion, device)
+        mape = evaluate_model(model, test_loader, mape_criterion, device)
         print(f"Epoch {epoch+1}/{epochs} - Train Loss: {total_loss / len(train_loader):.4f}")
+        print("Test results:")
+        print(f"Mean Absolute Error {mae}\nRoot Mean Squared Error {rmse}\nMean Absolute percentage error {mape}%\n")
 
     return model
 
@@ -36,3 +41,13 @@ def evaluate_model(model, dataloader, criterion, device):
 
     return total_loss / len(dataloader)
         
+def mae_criterion(pred : torch.tensor, value: torch.tensor):
+    return torch.mean(torch.abs(pred - value))
+
+def rmse_criterion(pred: torch.tensor, value: torch.tensor):
+    return torch.sqrt(torch.mean((pred - value) ** 2))
+
+def mape_criterion(pred: torch.tensor, value: torch.tensor):
+    eps = 1e-8
+    percentage_errors = torch.abs((pred - value) / torch.clamp(torch.abs(value), min=eps))
+    return torch.mean(percentage_errors) * 100 # may become unstable since values in the dataset are normalizes and close to zero
